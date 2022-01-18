59e547bee7a1f4235f0e9476eb2b6d63af59529f
￴ <@pop-os.localdomain>
2022-01-18 10:43:53 +0700

    update

--- node-js/github_client/test.js
+++ node-js/github_client/test.js
@@ -1,26 +1,30 @@
 var lib = require("./src/index");
 
 async function main() {
-    var gh = new lib.GithubClient("");
-    var data = await gh.getFollowers("Tahlreth", {
-        "page": 1
-    });
-    var berhasil = 0;
-    var gagal = 0;
-    var total = data.length;
-    
-    for (let index = 0; index < data.length; index++) {
-        const element = data[index];
-        try {
-            await gh.followAccountUser(element.login);
-            console.log("berhasil "+berhasil);
-            berhasil++;
-        } catch (e) {
-            console.log("gagal "+gagal);
-            gagal++;
+    var gh = new lib.GithubClient("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");
+    try {
+        var data = await gh.getFollowers("Tahlreth", {
+            "page": 1
+        });
+        var berhasil = 0;
+        var gagal = 0;
+        var total = data.length;
+
+        for (let index = 0; index < data.length; index++) {
+            const element = data[index];
+            try {
+                await gh.followAccountUser(element.login);
+                console.log("berhasil " + berhasil);
+                berhasil++;
+            } catch (e) {
+                console.log("gagal " + gagal);
+                gagal++;
+            }
         }
+        console.log(`berhasil ${berhasil}, gagal ${gagal}, ${total}`);
+    } catch (e) {
+
     }
-    console.log(`berhasil ${berhasil}, gagal ${gagal}, ${total}`);
 }
 
 main();
