6675b7509d15d726f46934469d43e57b755b4ed5
Your Name <you@example.com>
2022-10-13 07:12:01 +0700

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+# Specta Bot Telegram
---
+++ test/specta_bot_telegram_test.dart
@@ -0,0 +1,8 @@
+import 'package:specta_bot_telegram/specta_bot_telegram.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
